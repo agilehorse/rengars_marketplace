7fda0a9c2c97f5e81b43b82762aa3cd06a0d674c
Dávid Michal Bulko <bulkodav@fel.cvut.cz>
2021-05-31 18:59:12 +0200

    Add endpoint for eureka connection.

--- controllers/main_controller.py
+++ controllers/main_controller.py
@@ -1,2 +1,20 @@
+import connexion
+
+from env_vars import EUREKA_URL
+from services.ExternalServices import ExternalServices
+
+
 def hello():
     return "Hello", 200
+
+
+def eureka():
+    if not EUREKA_URL:
+        return connexion.problem(500, "Internal Server error", "Eureka url not set", "errors.eureka")
+    if not ExternalServices.eureka_on:
+        ExternalServices.init_eureka()
+        if not ExternalServices.eureka_on:
+            return connexion.problem(500, "Internal Server error",
+                                     "Unable to connect to eureka, the server might not be running", "errors.eureka")
+
+        return "OK", 200
